f0b28112f82a05b56d0c8ae29829e14292a92cf1
Samet <sametmacit@outlook.com>
2015-04-21 23:52:39 +0300

    ilk düzeltme

--- app/controllers/DersController.php
+++ app/controllers/DersController.php
@@ -5,6 +5,17 @@
  * Date: 13.05.2014
  * Time: 21:38
  */
+class TestController extends BaseController {
+
+    public function index($ders) {
+
+        $ders = Ders::where('title', '=', $ders)->first();
+        $articles = $ders->articles()->paginate(10);
+        $dersler = Ders::with('articles')->get();
+
+        return View::make('frontend.article.index', compact('articles','dersler'));
+    }
+
 class DersController extends BaseController {
 
     public function index($ders) {
