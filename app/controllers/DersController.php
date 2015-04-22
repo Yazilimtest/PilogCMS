181b3ecc172d14d12466befe63b621ceaef01945
Samet <sametmacit@outlook.com>
2015-04-22 09:39:06 +0300

    Update DersController.php

--- app/controllers/DersController.php
+++ app/controllers/DersController.php
@@ -6,16 +6,7 @@
  * Time: 21:38
  * 
     */
-    class Ders2Controller extends BaseController {
 
-    public function index($ders) {
-
-    $ders = Ders::where('title', '=', $ders)->first();
-    $articles = $ders->articles()->paginate(10);
-    $dersler = Ders::with('articles')->get();
-
-    return View::make('frontend.article.index', compact('articles','dersler'));
-}
 
 
 class DersController extends BaseController {
