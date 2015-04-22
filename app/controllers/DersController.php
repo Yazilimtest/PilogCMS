9eb5e04a389899c6b3f98f1fb7d5467fcadaa171
Samet <sametmacit@outlook.com>
2015-04-22 12:12:25 +0300

    Test2Controller

--- app/controllers/DersController.php
+++ app/controllers/DersController.php
@@ -6,6 +6,17 @@
  * Time: 21:38
  * 
     */
+	
+	class Test2Controller extends BaseController {
+
+    public function index($ders) {
+
+    $ders = Ders::where('title', '=', $ders)->first();
+    $articles = $ders->articles()->paginate(10);
+    $dersler = Ders::with('articles')->get();
+
+    return View::make('frontend.article.index', compact('articles','dersler'));
+}
 
 
 
